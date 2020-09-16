77d383fcdde2af5f72857db46783a6b0d9e391eb
AlexanderCm1 <victorcampos@upeu.edu.pe>
2020-09-16 12:12:10 -0500

    codigo

--- src/main/java/com/example/sysventas_EternalBlue/entity/Rol.java
+++ src/main/java/com/example/sysventas_EternalBlue/entity/Rol.java
@@ -1,5 +1,29 @@
 package com.example.sysventas_EternalBlue.entity;
 
 public class Rol {
+	private int idrol;
+	private String nombre;
+	public Rol() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Rol(int idrol, String nombre) {
+		super();
+		this.idrol = idrol;
+		this.nombre = nombre;
+	}
+	public int getIdrol() {
+		return idrol;
+	}
+	public void setIdrol(int idrol) {
+		this.idrol = idrol;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 
 }
